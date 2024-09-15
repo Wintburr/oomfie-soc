f5cb9b173ca0dd69ee65009ead186d072e3e9903
Wintburr <wintburr@oomfie.city>
2024-09-15 14:57:26 +0200

    upping the alt text char limit
    sorry screen reader users

--- app/models/media_attachment.rb
+++ app/models/media_attachment.rb
@@ -37,7 +37,7 @@ class MediaAttachment < ApplicationRecord
   enum :type, { image: 0, gifv: 1, video: 2, unknown: 3, audio: 4 }
   enum :processing, { queued: 0, in_progress: 1, complete: 2, failed: 3 }, prefix: true
 
-  MAX_DESCRIPTION_LENGTH = 1_500
+  MAX_DESCRIPTION_LENGTH = 15_000
 
   IMAGE_LIMIT = (ENV['MAX_IMAGE_SIZE'] || 100.megabytes).to_i
   VIDEO_LIMIT = (ENV['MAX_VIDEO_SIZE'] || 300.megabytes).to_i
